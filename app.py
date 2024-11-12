5e17ab9bb598fe2498dc7d4d84a8749d27ac31b2
mclint <mclint@uw.edu>
2024-11-12 10:43:53 -0800

    finished!

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import urllib.request
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def is_it_raining_in_seattle():
+    try:
+        with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
+            is_it_raining_in_seattle = response.read().decode()
+
+            if is_it_raining_in_seattle == "true":
+                return "<h1>Yes</h1>"
+            else:
+                return "<h1>No</h1>"
+    except Exception as e:
+        return "Error"
